075787f2455b36afb3983699db8265c10e255466
Carl Jenkins <carljenkins@gmail.com>
2016-08-13 08:31:52 -0500

    add lizard spock feature

--- 120_oop/lesson_2/oo_rps/move.rb
+++ 120_oop/lesson_2/oo_rps/move.rb
@@ -3,7 +3,7 @@ class Move
   include Comparable
 
   attr_accessor :value
-  VALUES = ['rock', 'paper', 'scissors'].freeze
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock'].freeze
 
   def initialize(move_value)
     self.value = move_value
@@ -14,15 +14,42 @@ class Move
   # +1  greater than
   def <=>(other)
     return nil unless other.is_a?(self.class)
-    if rock? && other.scissors? ||
-       paper? && other.rock?    ||
-       scissors? && other.paper?
+    if scissors_wins?(other) ||
+       spock_wins?(other)    ||
+       rock_wins?(other)     ||
+       paper_wins?(other)    ||
+       lizard_wins?(other)
       1
     elsif value == other.value
       0
     else
       -1
     end
+  end
+
+  def scissors_wins?(other)
+    scissors? && other.paper? ||
+      scissors? && other.spock?
+  end
+
+  def spock_wins?(other)
+    spock? && other.scissors? ||
+      spock? && other.rock?
+  end
+
+  def rock_wins?(other)
+    rock? && other.scissors? ||
+      rock? && other.lizard?
+  end
+
+  def paper_wins?(other)
+    paper? && other.rock? ||
+      paper? && other.spock?
+  end
+
+  def lizard_wins?(other)
+    lizard? && other.spock? ||
+      lizard? && other.paper?
   end
 
   def rock?
@@ -35,6 +62,14 @@ class Move
 
   def scissors?
     value == 'scissors'
+  end
+
+  def lizard?
+    value == 'lizard'
+  end
+
+  def spock?
+    value == 'spock'
   end
 
   def to_s
