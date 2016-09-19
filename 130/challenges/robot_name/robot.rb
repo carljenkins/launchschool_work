97bd1f2949963da5598b5ab5c53dc31cd191f4eb
Carl Jenkins <carljenkins@gmail.com>
2016-09-19 13:29:25 -0500

    completed Robot name challenge

---
+++ 130/challenges/robot_name/robot.rb
@@ -0,0 +1,40 @@
+class Robot
+  attr_accessor :name
+  @@used_names = []
+
+  def initialize
+    @name = ""
+    @name = set_name
+    @@used_names << @name
+  end
+
+  def self.used_names
+    @@used_names
+  end
+
+   def reset
+     set_name
+   end
+
+   private
+
+   def set_name
+     name = ""
+     loop do
+       name = generate_name
+       break unless Robot.used_names.include?(name)
+     end
+     self.name = name
+   end
+
+   def generate_name
+     result = ""
+     2.times do
+       result << rand((65..90)).chr
+     end
+     3.times do
+       result << rand((48..57)).chr.to_s
+     end
+     result
+   end
+end
