1a6b06e6d36f21d9ae58548b839c5182bb87e3d6
Carl Jenkins <carljenkins@gmail.com>
2016-09-30 04:46:24 -0500

    Various additions

--- 130/challenges/secret_handshake/secret_handshake.rb
+++ 130/challenges/secret_handshake/secret_handshake.rb
@@ -6,146 +6,51 @@
 # 10000 = Reverse the order of the operations in the secret handshake.
 # 128 - 64 - 32 - 16 - 8 - 4 - 2 - 1
 class SecretHandshake
-    COMMANDS = { '1' => 'wink', '10' => 'double blink', '100' => 'close your eyes', '1000' => 'jump'}
 
-  def initialize(handshake_code)
-    @decimal = handshake_code
+  BASE_2 = 2
+  COMMANDS = %w[wink double\ blink close\ your\ eyes jump]
+
+  def initialize(code)
+    @secret_code = code
   end
 
   def commands
-    # require 'pry'; binding.pry
-    binary = BinaryBuilder.new.convert(@decimal)
-    result = []
-    binary.each do | val |
-      result << COMMANDS[val]
+    return [] unless @secret_code.is_a? Integer
+    binary = @secret_code.to_s(BASE_2)
+    require 'pry'; binding.pry
+    # binary.reverse! if binary.chars.size >= 5
+    indexs = positions(binary).compact
+    require 'pry'; binding.pry
+
+    p indexs
+    arry = []
+    indexs.each do | val |
+      arry << COMMANDS.values_at(val)
     end
-    # require 'pry'; binding.pry
-    result
-  end
-
-end
-
-
-
-
-
-
-class BinaryBuilder
-  attr_accessor :numbers
-  BINARY_MULTIPLYER = 2
-
-  def initialize
-    @numbers = []
-  end
-
-  def <<(series)
-    raise ArgumentError, "1's and 0's only" unless valid_binary?(series)
-    @numbers.push series
-  end
-
-  def select
-    return @numbers.enum_for if !block_given?
-    binary = BinaryBuilder.new
-    each do | bi |
-      binary << bi if yield(bi)
-    end
-    binary
-  end
-
-  def each
-    return @numbers.enum_for if !block_given?
-    counter = 0
-    while counter < @numbers.size
-      yield(@numbers[counter])
-      counter += 1
-    end
-    self
-  end
-
-
-  def pop
-    @numbers.pop
-  end
-
-  def []=(idx, value)
-    raise ArgumentError, "1's and 0's only" unless valid_binary?(series)
-    @numbers[idx] = value
-  end
-
-  def [](idx)
-    @numbers[idx]
-  end
-
-  def decimal_value
-    value = 0
-    @numbers.reverse.each.with_index do | n, idx |
-      value += index_value(idx) if n == 1
-    end
-    value
-  end
-
-  def convert digit
-    values = []
-    loop do
-      remainder = nearest_power_of_2(digit)
-      values << remainder
-      digit = digit - remainder
-      break unless digit > 0
-    end
-    convert_to_binary_form(values)
-  end
-
-  def to_s
-    result = ''
-    @numbers.each do | v |
-      result << v + ' '
-    end
-    result
+    arry.flatten
   end
 
   private
 
-  def index_value(idx)
-    BINARY_MULTIPLYER ** idx
+  def positions(binary)
+    binary.chars.reverse.map.with_index{ | d, i |  i if d == '1'}
   end
 
-  def convert_to_binary_form(digits)
-    # require 'pry'; binding.pry
-    digits.each do | n |
-      bi_value = []
-      (0..n).collect.with_index do | idx |
-        bi_value[idx] =
-          if n == index_value(idx)
-            1
-          elsif index_value(idx) < n
-            0
-        end
-      end
-      # require 'pry'; binding.pry
-      @numbers << bi_value.compact!.join('').reverse
-    end
-    self
-  end
-
-  def nearest_power_of_2 number
-    return 0 if number <= 0
-    exponent = Math.log2 number
-    BINARY_MULTIPLYER ** exponent.floor
-  end
-
-  def valid_binary?(series)
-    series.chars.all? { | n | n == '1' || n == '0' }
-  end
 end
 
+# s = SecretHandshake.new(1)
+# p s.commands
+# s = SecretHandshake.new(2)
+# p s.commands
+# s = SecretHandshake.new(4)
+# p s.commands
+# s = SecretHandshake.new(8)
+# p s.commands
+# s = SecretHandshake.new(3)
+# p s.commands
+s = SecretHandshake.new(19)
+p s.commands
+# 'double blink', 'wink'
 
 
-shake = SecretHandshake.new(8)
-p shake.commands
-# b = BinaryBuilder.new
-# b.convert 8
-# puts b
-# bi_obj = b.select do | e |
-#   e == "1"
-# end
-# puts bi_obj
+  # expected = ['jump', 'close your eyes', 'double blink', 'wink']
