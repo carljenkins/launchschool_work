da0e43e4a368b6a63e7d7f3e21e0f2dac09130eb
Carl Jenkins <carljenkins@gmail.com>
2016-10-01 10:45:43 -0500

    complete secret handshake challenge

--- 130/challenges/secret_handshake/secret_handshake.rb
+++ 130/challenges/secret_handshake/secret_handshake.rb
@@ -1,10 +1,3 @@
-
-# 1 = wink
-# 10 = double blink
-# 100 = close your eyes
-# 1000 = jump
-# 10000 = Reverse the order of the operations in the secret handshake.
-# 128 - 64 - 32 - 16 - 8 - 4 - 2 - 1
 class SecretHandshake
 
   BASE_2 = 2
@@ -17,17 +10,12 @@ class SecretHandshake
   def commands
     return [] unless @secret_code.is_a? Integer
     binary = @secret_code.to_s(BASE_2)
-    require 'pry'; binding.pry
-    # binary.reverse! if binary.chars.size >= 5
-    indexs = positions(binary).compact
-    require 'pry'; binding.pry
 
-    p indexs
     arry = []
-    indexs.each do | val |
+    command_indexes(binary).each do | val |
       arry << COMMANDS.values_at(val)
     end
-    arry.flatten
+    binary.size >= 5 ? arry.flatten.reverse.compact : arry.flatten
   end
 
   private
@@ -36,21 +24,7 @@ class SecretHandshake
     binary.chars.reverse.map.with_index{ | d, i |  i if d == '1'}
   end
 
+  def command_indexes(binary)
+    indexs = positions(binary).compact
+  end
 end
-
-# s = SecretHandshake.new(1)
-# p s.commands
-# s = SecretHandshake.new(2)
-# p s.commands
-# s = SecretHandshake.new(4)
-# p s.commands
-# s = SecretHandshake.new(8)
-# p s.commands
-# s = SecretHandshake.new(3)
-# p s.commands
-s = SecretHandshake.new(19)
-p s.commands
-# 'double blink', 'wink'
-
-
-  # expected = ['jump', 'close your eyes', 'double blink', 'wink']
